b1c1d7430f44f7273edf79f5a8a837cf3b05afd8
0uts1de <975side@gmail.com>
2023-12-01 09:51:42 +0900

    a

--- sketch_oct5a.ino
+++ sketch_oct5a.ino
@@ -6,7 +6,7 @@
 #include "CdS.hpp"
 
 //----------START pin numbers----------
-const int VL53L1X_count=3;
+const int VL53L1X_count = 3;
 const int VL53L1X_XSHUT[VL53L1X_count] = {2, 3, 4};
 // digital
 
@@ -18,28 +18,32 @@ const int L298N_IN4=13;
 const int L298N_ENB=11;
 // digital
 
-const int CdS_PIN[2] = {0, 1};
+const int CdS_count = 2;
+const int CdS_PIN[CdS_count] = {0, 1};
 // analog
 //----------END pin numbers----------
 
 //----------START prototype----------
 void linetrace();
-void ultrasonic();
+void noline();
 void obstacle();
 void testrun();
 //----------END prototype----------
 
 //----------START global var----------
-L298N_ L298N;
-VL53L1X VL53L1X_[VL53L1X_count];  // 命名規則が...  おのれ VL53L1X.h
-CdS_ CdS[2];
 const auto boot_time = millis();
+L298N_ L298N;
+VL53L1X VL53L1X_[VL53L1X_count];
+CdS_ CdS[CdS_count];
 uint16_t dist[VL53L1X_count] = {0, 0, 0};
 bool line[2] = {false, false};
 //----------END global var----------
 
 void setup() {
-  if (DEBUG) Serial.begin(115200);
+  if (DEBUG) {
+    Serial.begin(115200);
+    Serial.println("Setup begin.");
+  }
   Wire.begin();
   Wire.setClock(400000); // use 400 kHz I2C
 
@@ -49,17 +53,16 @@ void setup() {
   }
   for (size_t i = 0; i < VL53L1X_count; ++i) {
     delay(50);
-    Serial.print("a");
     // pinMode(VL53L1X_XSHUT[i], INPUT);
     digitalWrite(VL53L1X_XSHUT[i], HIGH);
     delay(50);
-    Serial.print("b");
     VL53L1X_[i].setTimeout(50);
     VL53L1X_[i].init();
     if (!VL53L1X_[i].init()) {
       Serial.print("Failed to detect and initialize sensor ");
       Serial.println(i);
-      while (true){};
+      Serial.print("Retrying...");
+      asm volatile ("  jmp 0");
     }
     VL53L1X_[i].setAddress(0x2A + i);
     VL53L1X_[i].setDistanceMode(VL53L1X::Medium);
@@ -69,41 +72,36 @@ void setup() {
 
   L298N.attach(L298N_IN1, L298N_IN2, L298N_ENA, L298N_IN3, L298N_IN4, L298N_ENB);
   
-  for (size_t i = 0; i < 2; ++i) {
+  for (size_t i = 0; i < CdS_count; ++i) {
     CdS[i].attach(CdS_PIN[i]);
   }
 
-  Serial.print("setup done.");  
+  Serial.print("Setup done.");  
 }
 
-void loop() {
-
-  if (DEBUG) {
-    // char s[128];
-    // sprintf(s, "CdS_left: %s  CdS_right: %s  ", line[0] ? "true" : "false", line[1] ? "true" : "false");
-    // Serial.print(s);
-    // Serial.print("SR04_left: ");
-    // Serial.print(dist[0]);
-    // Serial.print("  SR04_right: ");
-    // Serial.print(dist[1]);
-    // Serial.print("  SR04_front: ");
-    // Serial.print(dist[2]);
-    // Serial.print("\n");
-    for (size_t i = 0; i < VL53L1X_count; ++i) {
-      Serial.println(VL53L1X_[i].read());
-    }
-    
-  }
-  
+void loop() {  
   if (TESTMODE) testrun();
 
-  line[0] = CdS[0].get_onblackline(); // left
-  line[1] = CdS[1].get_onblackline(); // right
+  for (size_t i = 0; i < CdS_count; ++i) {
+    line[i] = CdS[i].get_onblackline(); // l, r
+  }
+  
   if (line[0] == true || line[1] == true) {
     linetrace();
   }
   else {
-    ultrasonic();
+    noline();
+  }
+
+  if (DEBUG) {
+    String s;
+    for (size_t i = 0; i < CdS_count; ++i) {
+      s += "CdS_" + String(i) + line[0] ? "true" : "false";
+    }
+    for (size_t i = 0; i < VL53L1X_count; ++i) {
+      s += "VL53L1X_" + String(i) + String(dist[i]);
+    }
+    Serial.println(s);
   }
 
   delay(DELAY);
@@ -124,8 +122,9 @@ void linetrace() {
       }
     }
 
-    line[0] = CdS[0].get_onblackline(); // left
-    line[1] = CdS[1].get_onblackline(); // right
+    for (size_t i = 0; i < CdS_count; ++i) {
+      line[i] = CdS[i].get_onblackline(); // l, r
+    }
     if (line[0] == true && line[1] == true) {
       L298N.move_front(SPEED);
       lost_flag = false;
@@ -154,32 +153,27 @@ void linetrace() {
       }
     }
     if (DEBUG) {
-      char s[128];
-      sprintf(s, "CdS_left: %s  CdS_right: %s  ", line[0] ? "true" : "false", line[1] ? "true" : "false");
-      Serial.print(s);
-      for (size_t i = 0; i < VL53L1X_count; ++i) {
-        Serial.println(VL53L1X_[i].read());
+      String s;
+      for (size_t i = 0; i < CdS_count; ++i) {
+        s += "CdS_" + String(i) + line[0] ? "true" : "false";
       }
+      for (size_t i = 0; i < VL53L1X_count; ++i) {
+        s += "VL53L1X_" + String(i) + String(dist[i]);
+      }
+      Serial.println(s);
     }
   }
 }
 
-void ultrasonic() {
-  dist[0] = VL53L1X_[0].read(); // left
-  dist[1] = VL53L1X_[1].read(); // right
-  dist[2] = VL53L1X_[2].read(); // front
-
+void noline() {
+  for (size_t i = 0; i < VL53L1X_count; ++i) {
+    dist[i] = VL53L1X_[i].read(); // l, r, f
+    if (dist[i] < 0) dist[i] = 999;
+  }
   
-
-  if (dist[0] < 0) dist[0] = 999;
-  if (dist[1] < 0) dist[1] = 999;
-  if (dist[2] < 0) dist[2] = 999;
-
-
-  if (dist[2] < 100) {
+  if (dist[2] < 100) { // 前方障害物で右折
     L298N.turn_right(SPEED);
   }
-
 
   if (dist[0] < dist[1]) {
     L298N.left_wheel(SPEED * 0.8);
@@ -189,35 +183,19 @@ void ultrasonic() {
     L298N.left_wheel(SPEED * 0.4);
     L298N.right_wheel(SPEED * 0.8);
   }
-
-  // if (dist[0] < dist[1]) {
-  //   if (dist[0] < 100) {
-  //     L298N.left_wheel(SPEED * 0.8);
-  //     L298N.right_wheel(0);
-  //   }
-  //   else {
-  //     L298N.left_wheel(0);
-  //     L298N.right_wheel(SPEED * 0.8);
-  //   }
-  // }
-  // else {
-  //   if (dist[1] < 100) {
-  //     L298N.left_wheel(0);
-  //     L298N.right_wheel(SPEED * 0.8);
-  //   }
-  //   else {
-  //     L298N.left_wheel(SPEED * 0.8);
-  //     L298N.right_wheel(0);
-  //   }
-  // }
-  
 }
 
 void obstacle() {
-  while (true)
-  {
-    L298N.stop();
-    if (VL53L1X_[2].read() > 150) break;
+  while (VL53L1X_[0].read() > 150) {
+    L298N.turn_right(SPEED);
+  }
+  while (CdS[0].get_onblackline() || CdS[1].get_onblackline()) {
+    while (VL53L1X_[0].read() < 150) {
+      L298N.move_front(SPEED);
+    }
+    while (VL53L1X_[0].read() > 150) {
+      L298N.turn_left(SPEED);
+    }
   }
 }
 
