76b32585656f2c00dd806c3e1ad0c45173ab5808
0uts1de <975side@gmail.com>
2023-12-08 13:14:10 +0900

    距離センサの種類を切り替えられるように

---
+++ dist_sensor.hpp
@@ -0,0 +1,161 @@
+#pragma once
+
+#include <VL53L1X.h>
+#include <Wire.h>
+#include "SR04.hpp"
+#include "constant.hpp"
+
+bool flag_for_VL53L1X_setup_ = false;
+
+class DIST_SENSOR_;
+void VL53L1X_setup(DIST_SENSOR_ *, const int);
+
+class DIST_SENSOR_
+{
+    friend void VL53L1X_setup(DIST_SENSOR_ *, const int);
+
+public:
+    DIST_SENSOR_(){};
+    DIST_SENSOR_(int PIN_XSHUT)
+    {
+        if (DISTANCE_SENSOR_MODE != 1)
+            return;
+    }
+    DIST_SENSOR_(int PIN_TRIG, int PIN_ECHO)
+    {
+        if (DISTANCE_SENSOR_MODE != 0)
+            return;
+        this->SR04_attach_(PIN_TRIG, PIN_ECHO);
+    }
+    ~DIST_SENSOR_(){};
+
+    void attach(int PIN_XSHUT)
+    {
+        if (DISTANCE_SENSOR_MODE != 1)
+            return;
+        this->VL53L1X_attach_(PIN_XSHUT);
+    }
+
+    void attach(int PIN_TRIG, int PIN_ECHO)
+    {
+        if (DISTANCE_SENSOR_MODE != 0)
+            return;
+        this->SR04_attach_(PIN_TRIG, PIN_ECHO);
+    }
+
+    double get_distance()
+    {
+        if (DISTANCE_SENSOR_MODE == 0)
+            return this->SR04_get_distance_();
+        else if (DISTANCE_SENSOR_MODE == 1)
+            return this->VL53L1X_.read();
+    }
+
+private:
+    int TRIG_, ECHO_;
+
+    void SR04_(int TRIG, int ECHO)
+    {
+        this->SR04_attach_(TRIG, ECHO);
+    }
+
+    void SR04_attach_(int TRIG, int ECHO)
+    {
+        TRIG_ = TRIG;
+        ECHO_ = ECHO;
+        this->SR04_setup_();
+    }
+
+    double SR04_get_distance_()
+    {
+        // return distance in [mm]
+        // if failed return -1
+        double duration = 0;
+        double distance = 0;
+
+        digitalWrite(TRIG_, LOW);
+        delayMicroseconds(2);
+        digitalWrite(TRIG_, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(TRIG_, LOW);
+        duration = pulseIn(ECHO_, HIGH, TIMEOUT);
+
+        if (duration > 0)
+        {
+            duration /= 2;
+            distance = duration * speed_of_sound * 1000 / 1000000;
+        }
+        else
+        {
+            distance = -1;
+        }
+
+        return distance;
+    }
+
+    void SR04_setup_()
+    {
+        pinMode(TRIG_, OUTPUT);
+        pinMode(ECHO_, INPUT);
+    }
+
+    int XSHUT_;
+    VL53L1X VL53L1X_;
+    void VL53L1X_attach_(int XSHUT)
+    {
+        XSHUT_ = XSHUT;
+    }
+};
+
+void VL53L1X_setup(DIST_SENSOR_ *DIST_SENSOR, const int VL53L1X_count)
+{
+    if (DISTANCE_SENSOR_MODE != 1)
+    {
+        return;
+    }
+
+    Wire.begin();
+
+    if (DEBUG)
+        Serial.println("I2C started.");
+
+    for (size_t i = 0; i < VL53L1X_count; ++i)
+    {
+        pinMode(DIST_SENSOR[i].XSHUT_, OUTPUT);
+        digitalWrite(DIST_SENSOR[i].XSHUT_, LOW);
+    }
+
+    if (DEBUG)
+        Serial.println("All sensors shutted down.");
+
+    for (size_t i = 0; i < VL53L1X_count; ++i)
+    {
+        // digitalWrite(VL53L1X_XSHUT[i], HIGH);
+        pinMode(DIST_SENSOR[i].XSHUT_, INPUT);
+        delay(10);
+        DIST_SENSOR[i].VL53L1X_.setTimeout(500);
+        if (DIST_SENSOR[i].VL53L1X_.init() == false)
+        {
+            if (DEBUG)
+            {
+                Serial.print("Failed to detect and initialize sensor ");
+                Serial.println(i);
+                Serial.println("Retrying...");
+            }
+            delay(100);
+            asm volatile("  jmp 0");
+        }
+        else
+        {
+            if (DEBUG)
+            {
+                Serial.print("Initialized VL53L1X_");
+                Serial.println(i);
+            }
+        }
+        DIST_SENSOR[i].VL53L1X_.setAddress(0x2A + i * 2);
+        // VL53L1X_.setDistanceMode(VL53L1X::Long);
+        // VL53L1X_.setMeasurementTimingBudget(5000);
+        DIST_SENSOR[i].VL53L1X_.startContinuous(50);
+    }
+}
--- sketch_oct5a.ino
+++ sketch_oct5a.ino
@@ -1,21 +1,22 @@
 #include "constant.hpp"
 
 #include <Wire.h>
-#include <VL53L1X.h>
+#include "dist_sensor.hpp"
 #include "L298N.hpp"
 #include "CdS.hpp"
 
 //----------START pin numbers----------
-const int VL53L1X_count = 3;
-const int VL53L1X_XSHUT[VL53L1X_count] = {2, 3, 4};
+const int dist_sensor_count = 3;
+const int SR04_PIN[dist_sensor_count * 2] = {2, 3, 4, 5, 6, 7};
+const int VL53L1X_XSHUT[dist_sensor_count] = {2, 3, 4};
 // digital
 
-const int L298N_IN1=8;
-const int L298N_IN2=9;
-const int L298N_ENA=10;
-const int L298N_IN3=12;
-const int L298N_IN4=13;
-const int L298N_ENB=11;
+const int L298N_IN1 = 8;
+const int L298N_IN2 = 9;
+const int L298N_ENA = 10;
+const int L298N_IN3 = 12;
+const int L298N_IN4 = 13;
+const int L298N_ENB = 11;
 // digital
 
 const int CdS_count = 2;
@@ -34,214 +35,232 @@ void print_debug();
 //----------START global var----------
 const auto boot_time = millis();
 L298N_ L298N;
-VL53L1X VL53L1X_[VL53L1X_count];
+DIST_SENSOR_ DIST_SENSOR[dist_sensor_count];
+// VL53L1X VL53L1X_[VL53L1X_count];
 CdS_ CdS[CdS_count];
-uint16_t dist[VL53L1X_count] = {0, 0, 0};
-bool line[2] = {false, false};
+uint16_t dist[dist_sensor_count] = {0, 0, 0};
+bool line[CdS_count] = {false, false};
 //----------END global var----------
 
-void setup() {
-  Serial.begin(115200);
-  if (DEBUG) Serial.println("Setup begin.");
-  
-  Wire.begin();
+void setup()
+{
+    Serial.begin(115200);
+    if (DEBUG)
+        Serial.println("Setup begin.");
 
-  if (DEBUG) Serial.println("I2C started.");
-
-  for (size_t i = 0; i < VL53L1X_count; ++i) {
-    pinMode(VL53L1X_XSHUT[i], OUTPUT);
-    digitalWrite(VL53L1X_XSHUT[i], LOW);
-  }
-
-  if (DEBUG) Serial.println("All sensors shutted down.");
-  
-  for (size_t i = 0; i < VL53L1X_count; ++i) { 
-    //digitalWrite(VL53L1X_XSHUT[i], HIGH);
-    pinMode(VL53L1X_XSHUT[i], INPUT);
-    delay(10);
-    VL53L1X_[i].setTimeout(500);   
-    if (VL53L1X_[i].init() == false) {
-      if (DEBUG) {
-        Serial.print("Failed to detect and initialize sensor ");
-        Serial.println(i);
-        Serial.print("Retrying...");
-      }
-      delay(100);
-      asm volatile ("  jmp 0");
+    for (size_t i = 0; i < dist_sensor_count; ++i)
+    {
+        if (DISTANCE_SENSOR_MODE == 0)
+            DIST_SENSOR[i].attach(SR04_PIN[i * 2], SR04_PIN[i * 2 + 1]);
+        else if (DISTANCE_SENSOR_MODE == 1)
+            DIST_SENSOR[i].attach(VL53L1X_XSHUT[i]);
     }
-    else {
-      if (DEBUG) {
-        Serial.print("Initialized VL53L1X_");
-        Serial.println(i);
-      }
+    if (DISTANCE_SENSOR_MODE == 1)
+        VL53L1X_setup(DIST_SENSOR, dist_sensor_count);
+
+    L298N.attach(L298N_IN1, L298N_IN2, L298N_ENA, L298N_IN3, L298N_IN4, L298N_ENB);
+
+    for (size_t i = 0; i < CdS_count; ++i)
+    {
+        CdS[i].attach(CdS_PIN[i]);
     }
-    VL53L1X_[i].setAddress(0x2A + i * 2);
-    // VL53L1X_[i].setDistanceMode(VL53L1X::Long);
-    // VL53L1X_[i].setMeasurementTimingBudget(5000);
-    VL53L1X_[i].startContinuous(50);   
-  }
 
-  L298N.attach(L298N_IN1, L298N_IN2, L298N_ENA, L298N_IN3, L298N_IN4, L298N_ENB);
-  
-  for (size_t i = 0; i < CdS_count; ++i) {
-    CdS[i].attach(CdS_PIN[i]);
-  }
-
-  if (DEBUG) Serial.println("Setup done.");  
+    if (DEBUG)
+        Serial.println("Setup done.");
 }
 
-void loop() {  
-  if (TESTMODE) testrun();
+void loop()
+{
+    if (TESTMODE)
+        testrun();
 
-  for (size_t i = 0; i < CdS_count; ++i) {
-    line[i] = CdS[i].get_onblackline(); // l, r
-  }
-  
-  if (line[0] == true || line[1] == true) {
-    linetrace();
-  }
-  else {
-    noline();
-  }
+    for (size_t i = 0; i < CdS_count; ++i)
+    {
+        line[i] = CdS[i].get_onblackline(); // l, r
+    }
 
-  print_debug();
+    if (line[0] == true || line[1] == true)
+    {
+        linetrace();
+    }
+    else
+    {
+        noline();
+    }
+
+    print_debug();
 }
 
-void linetrace() {
-  unsigned long lost_time = 0, d = 0, last_d = 0;
-  bool lr = 0; // 0: left  1: right
-  bool lost_flag = false;
-  while (true) {
-    d = (millis() - boot_time) / 250;
-    if (last_d == 0) last_d = d;
-    if (d != last_d) {
-      //dist[2] = VL53L1X_[2].read(); // front
-      if (dist[2] > 0 && dist[2] < 150) {
-        obstacle();
-        break;
-      }
-    }
+void linetrace()
+{
+    unsigned long lost_time = 0, d = 0, last_d = 0;
+    bool lr = 0; // 0: left  1: right
+    bool lost_flag = false;
+    while (true)
+    {
+        d = (millis() - boot_time) / 250;
+        if (last_d == 0)
+            last_d = d;
+        if (d != last_d)
+        {
+            // dist[2] = VL53L1X_[2].read(); // front
+            if (dist[2] > 0 && dist[2] < 150)
+            {
+                obstacle();
+                break;
+            }
+        }
 
-    for (size_t i = 0; i < CdS_count; ++i) {
-      line[i] = CdS[i].get_onblackline(); // l, r
+        for (size_t i = 0; i < CdS_count; ++i)
+        {
+            line[i] = CdS[i].get_onblackline(); // l, r
+        }
+        if (line[0] == true && line[1] == true)
+        {
+            L298N.move_front(SPEED);
+            lost_flag = false;
+        }
+        else if (line[0] == true)
+        {
+            L298N.right_wheel(SPEED);
+            L298N.left_wheel(SPEED * 0.5);
+            lr = 0;
+            lost_flag = false;
+        }
+        else if (line[1] == true)
+        {
+            L298N.left_wheel(SPEED);
+            L298N.right_wheel(SPEED * 0.5);
+            lr = 1;
+            lost_flag = false;
+        }
+        else
+        {
+            if (lost_flag == false)
+                lost_time = millis();
+            lost_flag = true;
+            if (lost_time - millis() > 2000)
+                break;
+            if (lr == 0)
+            {
+                L298N.turn_left(SPEED);
+            }
+            else
+            {
+                L298N.turn_right(SPEED);
+            }
+        }
+
+        print_debug();
     }
-    if (line[0] == true && line[1] == true) {
-      L298N.move_front(SPEED);
-      lost_flag = false;
+}
+
+void noline()
+{
+    bool first = true;
+    const double Kp = 20, Ki = 0.5, Kd = 0.5;
+    unsigned long t[2] = {0, 0};
+    long d[2] = {0, 0};
+    double integral = 0, control = 0;
+
+    while (!CdS[0].get_onblackline() && !CdS[1].get_onblackline())
+    {
+        for (size_t i = 0; i < dist_sensor_count; ++i)
+        {
+            dist[i] = DIST_SENSOR[i].get_distance(); // l, r, f
+            if (dist[i] < 0)
+                dist[i] = 999;
+        }
+
+        if (dist[2] < 50)
+        { // 前方障害物で後退
+            L298N.move_back(SPEED);
+        }
+        else
+        {
+            L298N.move_front(SPEED);
+        }
+
+        d[1] = d[0];
+        t[1] = t[0];
+        t[0] = millis();
+        d[0] = dist[0] - dist[1];
+        control = 0;
+        control += Kp * d[0];
+        if (!first)
+        {
+            integral += (d[0] + d[1]) * (t[0] - t[1]) / 2;
+            control += Ki * (d[0] + d[1]) * (t[0] - t[1]) / 2;
+            control += Kd * (d[0] - d[1]) / (t[0] - t[1]);
+        }
+
+        if (control > 1)
+            control = 1;
+
+        if (dist[0] < dist[1])
+        {
+            L298N.left_wheel(SPEED * control);
+            L298N.right_wheel(0);
+        }
+        else
+        {
+            L298N.right_wheel(SPEED * control);
+            L298N.left_wheel(0);
+        }
+        first = false;
+
+        print_debug();
     }
-    else if (line[0] == true) {
-      L298N.right_wheel(SPEED);
-      L298N.left_wheel(SPEED * 0.5);
-      lr = 0;
-      lost_flag = false;
-    }
-    else if (line[1] == true) {
-      L298N.left_wheel(SPEED);
-      L298N.right_wheel(SPEED * 0.5);
-      lr = 1;
-      lost_flag = false;
-    }
-    else {
-      if (lost_flag == false) lost_time = millis();
-      lost_flag = true;
-      if (lost_time - millis() > 2000) break;
-      if (lr == 0) {
-        L298N.turn_left(SPEED);
-      }
-      else {
+}
+
+void obstacle()
+{
+    while (DIST_SENSOR[0].get_distance() > 150)
+    {
         L298N.turn_right(SPEED);
-      }
+        print_debug();
     }
-    
-    print_debug();
-    
-  }
+    while (!CdS[0].get_onblackline() && !CdS[1].get_onblackline())
+    {
+        while (DIST_SENSOR[0].get_distance() < 150)
+        {
+            L298N.move_front(SPEED);
+            print_debug();
+        }
+        while (DIST_SENSOR[0].get_distance() > 150)
+        {
+            L298N.turn_left(SPEED);
+            print_debug();
+        }
+    }
 }
 
-void noline() {
-  bool first = true;
-  const double Kp = 20, Ki = 0.5, Kd = 0.5;
-  unsigned long t[2] = {0, 0};
-  long d[2] = {0, 0};
-  double integral = 0, control = 0;
-  
-  while (!CdS[0].get_onblackline() && !CdS[1].get_onblackline()) {
-    for (size_t i = 0; i < VL53L1X_count; ++i) {
-      dist[i] = VL53L1X_[i].read(); // l, r, f
-      if (dist[i] < 0) dist[i] = 999;
+void testrun()
+{
+    while (true)
+    {
+        auto d = millis() - boot_time;
+        if ((d / 2000) % 2)
+            L298N.turn_left(SPEED);
+        else
+            L298N.turn_right(SPEED);
+        print_debug();
     }
-  
-    if (dist[2] < 50) { // 前方障害物で後退
-      L298N.move_back(SPEED);
-    }
-    else {
-      L298N.move_front(SPEED);
-    }
-
-    d[1] = d[0];
-    t[1] = t[0];
-    t[0] = millis();
-    d[0] = dist[0] - dist [1];
-    control = 0;
-    control += Kp * d[0];
-    if (!first) {
-      integral += (d[0] + d[1]) * (t[0] - t[1]) / 2;
-      control += Ki * (d[0] + d[1]) * (t[0] - t[1]) / 2;
-      control += Kd * (d[0] - d[1]) / (t[0] - t[1]);
-    }
-
-    if (control > 1) control=1;
-
-    if (dist[0] < dist[1]) {
-      L298N.left_wheel(SPEED * control);
-      L298N.right_wheel(0);
-    }
-    else {
-      L298N.right_wheel(SPEED * control);
-      L298N.left_wheel(0);
-    }
-    first = false;
-
-    print_debug();
-  }
 }
 
-void obstacle() {
-  while (VL53L1X_[0].read() > 150) {
-    L298N.turn_right(SPEED);
-    print_debug();
-  }
-  while (!CdS[0].get_onblackline() && !CdS[1].get_onblackline()) {
-    while (VL53L1X_[0].read() < 150) {
-      L298N.move_front(SPEED);
-      print_debug();
+void print_debug()
+{
+    if (DEBUG)
+    {
+        String s;
+        for (size_t i = 0; i < CdS_count; ++i)
+        {
+            s += "CdS_" + String(i) + line[i] ? "true  " : "false ";
+        }
+        for (size_t i = 0; i < dist_sensor_count; ++i)
+        {
+            s += "\tdist_" + String(i) + ": " + String(dist[i]);
+        }
+        Serial.println(s);
     }
-    while (VL53L1X_[0].read() > 150) {
-      L298N.turn_left(SPEED);
-      print_debug();
-    }
-  }
-}
-
-void testrun() {
-  while (true)
-  {
-    auto d = millis() - boot_time;
-    if ((d / 2000)%2) L298N.turn_left(SPEED);
-    else L298N.turn_right(SPEED);
-    print_debug();
-  }  
-}
-
-void print_debug() {
-  if (DEBUG) {
-    String s;
-    for (size_t i = 0; i < CdS_count; ++i) {
-      s += "CdS_" + String(i) + line[i] ? "true  " : "false ";
-    }
-    for (size_t i = 0; i < VL53L1X_count; ++i) {
-      s += "\tVL53L1X_" + String(i) + ": " + String(dist[i]);
-    }
-    Serial.println(s);
-  }
 }
